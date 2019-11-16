ce7e06babcbe55417c81a878e061b0b77691a47f
Niek <git@niekvantoepassing.nl>
2019-11-16 21:28:05 +0100

    Create landing page and navbar

--- WordMask/Controllers/HomeController.cs
+++ WordMask/Controllers/HomeController.cs
@@ -1,100 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using WordMask.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WordMask.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly TheDictionary _dictionary;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-            _dictionary = new TheDictionary();
-        }
-
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
-
-        private Dictionary<char, char> GetAssociations(string input)
-        {
-            Dictionary<char, char> output = new Dictionary<char, char>();
-            foreach (var l in input.Split(",").Select(l => l.Trim().ToUpper()).ToList())
-            {
-                var association = l.Split("=");
-                if (association.Length == 2)
-                {
-                    var a = char.Parse(association[0].Substring(0, 1));
-                    var b = char.Parse(association[1].Substring(0, 1));
-                    if (!output.ContainsKey(a))
-                    {
-                        output.Add(a, b);
-                    }
-                }
-            }
-
-            return output;
-        }
-
-        private string GetReplacedWord(string mask, Dictionary<char, char> associations)
-        {
-            string replaced = "";
-            for (var i = 0; i < mask.Length; i++)
-            {
-                if (associations.ContainsKey(mask[i]))
-                {
-                    replaced += associations[mask[i]];
-                }
-                else
-                {
-                    replaced += "?";
-                }
-            }
-
-            return replaced;
-        }
-
-        [HttpPost]
-        public IActionResult Index(string masks, string notContains = "", string contains = "", string knownLetters = "")
-        {
-            notContains ??= "";
-            contains ??= "";
-            knownLetters ??= "";
-            var associations = GetAssociations(knownLetters);
-
-            ViewBag.Masks = masks;
-            ViewBag.NotContains = notContains;
-            ViewBag.Contains = contains;
-            ViewBag.KnownLetters = knownLetters;
-            ViewBag.Automode = Request.Form["notcontains-automode"] == "on";
-
-            ViewBag.Matches = new Dictionary<string, List<string>>();
-            ViewBag.ReplacedWord = new Dictionary<string, string>();
-            foreach (var mask in masks.Split(" "))
-            {
-                var ruleset = new Ruleset(
-                    mask,
-                    notContains.Split(",").Select(l => l.Trim().ToUpper()).ToList(),
-                    contains.Split(",").Select(l => l.Trim().ToUpper()).ToList(),
-                    associations
-                );
-            
-                ViewBag.Matches[mask] = _dictionary.FindMatches(ruleset);
-                ViewBag.ReplacedWord[mask] = GetReplacedWord(mask, associations);
-            }
-
-            return View();
-        }
-
-
     }
 }
