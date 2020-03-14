aec4fc8fbe5fea725f4a4e54ff05d291f8f63e2d
Niek <git@niekvantoepassing.nl>
2020-03-14 19:06:37 +0100

    Add a formula converter

---
+++ GeocachingToolbelt/Controllers/FormulaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeocachingToolbelt.Controllers
+{
+    [Route("formule")]
+    public class FormulaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
