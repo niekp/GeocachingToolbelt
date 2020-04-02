7593131d6163845264eb0e225767f6c1129a6a2c
Niek <git@niekvantoepassing.nl>
2020-04-02 19:35:45 +0200

    Start with substitution cypher stuff

---
+++ GeocachingToolbelt/wwwroot/js/substitution.js
@@ -0,0 +1,25 @@
+﻿(function ($) {
+    var encoded = '',
+        decoded = '';
+
+    $(document).ready(function () {
+        $("[data-id='encoded-text']").keyup(updateText);
+    });
+
+    var updateText = function () {
+        encoded = $("[data-id='encoded-text']").val();
+
+        decodeText();
+        displayResult();    
+    }
+
+    var decodeText = function () {
+        console.log(encoded)
+        decoded = encoded;
+    };
+
+    var displayResult = function () {
+        $("[data-id='decoded-text']").html(decoded);
+    };
+
+})(jQuery);
