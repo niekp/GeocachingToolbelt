911d77286b4fd472ece51be6bfae689544f9373a
Niek <git@niekvantoepassing.nl>
2020-07-29 19:00:20 +0200

    Add projection tool

---
+++ GeocachingToolbelt/Controllers/ProjectionController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeocachingToolbelt.Models;
+using GeocachingToolbelt.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeocachingToolbelt.Controllers
+{
+    [Route("projectie")]
+    [Route("projecteren")]
+    public class ProjectionController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string coord, double distance, double angle)
+        {
+            Coordinate coordinate;
+            try
+            {
+                coordinate = new Coordinate(coord);
+            }
+            catch (ArgumentException)
+            {
+                ViewBag.Error = "Coordinaat niet herkend";
+                return View();
+            }
+            ViewBag.Distance = distance;
+            ViewBag.Angle = angle;
+            ViewBag.Coordinate = coordinate;
+            ViewBag.Projected = coordinate.Project(distance, angle);
+
+            return View();
+        }
+    }
+}
